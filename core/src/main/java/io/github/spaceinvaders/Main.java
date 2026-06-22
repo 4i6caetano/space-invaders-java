cac8a78fd65679f219afb9fb31d73206b99faabf
4i6 <joaopcaetanoc@gmail.com>
2026-06-22 19:28:52 -0300

    feat:initiated the draw method -  background texture is shown on screen and playerSprite is shown on the left side of the screen.

--- core/src/main/java/io/github/spaceinvaders/Main.java
+++ core/src/main/java/io/github/spaceinvaders/Main.java
@@ -4,8 +4,11 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.Gdx;
+
+import com.badlogic.gdx.graphics.Color;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
@@ -13,7 +16,7 @@ public class Main extends Game {
     FitViewport viewport;
     SpriteBatch spriteBatch;
 
-    Texture backgroundImage;
+    Texture backgroundTexture;
 
     Sprite alien1Instance1;
     Sprite alien1Instance2;
@@ -33,7 +36,7 @@ public class Main extends Game {
 
         spriteBatch = new SpriteBatch();
 
-        backgroundImage = new Texture("background/backgroundTexture.png");
+        backgroundTexture = new Texture("background/backgroundTexture.png");
 
         Texture alien1Texture1 = new Texture("sprites/alien1Instance1.png");
         alien1Instance1 = new Sprite(alien1Texture1);
@@ -55,6 +58,11 @@ public class Main extends Game {
 
         Texture playerTexture = new Texture("sprites/playerSprite.png");
         playerSprite = new Sprite(playerTexture);
+
+        // Resizes and adjustments on sprites
+
+        playerSprite.setSize(70, 50);
+        playerSprite.setPosition(1, 1);
     }
 
     @Override
@@ -65,7 +73,7 @@ public class Main extends Game {
 
     public void render()
     {
-
+        draw();
     }
 
     public void resume()
@@ -81,5 +89,23 @@ public class Main extends Game {
     public void dispose()
     {
 
+    }
+
+    // these live inside the 'render' method.
+
+    private void draw()
+    {
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+        spriteBatch.begin();
+
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+        spriteBatch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight);
+        playerSprite.draw(spriteBatch);
+
+        spriteBatch.end();
     }
 }
